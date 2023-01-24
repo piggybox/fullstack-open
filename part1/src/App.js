7bd400cd4a2fe6741751cd20626ad3e9ea7a3aea
Sky Yin <sky.yin@gmail.com>
2023-01-24 06:05:23 -0800

    Simple math in react

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
 
 const App = () => {
+  const now = new Date();
+  const a = 10;
+  const b = 20;
+  console.log(now, a + b);
+
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
     </div>
   );
 };
