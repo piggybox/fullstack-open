00a00e0ef6f2a4a334c7eaecf73fef9c065a43be
Sky Yin <sky.yin@gmail.com>
2023-03-05 16:31:45 -0800

    [part1] props destruction

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,15 +1,20 @@
 import "./App.css";
 
-const Hello = (props) => {
-  console.log(props);
+const Hello = ({ name, age }) => {
+  // const { name, age } = props;
+
+  const bornYear = () => new Date().getFullYear() - age;
+
   return (
     <div>
       <p>
-        Hello {props.name}, you are {props.age} years old
+        Hello {name}, you are {age} years old
       </p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   );
 };
+
 const App = () => {
   const name = "Peter";
   const age = 10;
